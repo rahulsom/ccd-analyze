fba249d8af9094c61dd47040c9d063b7b099f35c
Rahul Somasunderam <rsom@certifydatasystems.com>
2014-10-28 13:13:33 -0700

    Fixed path for CCDs

--- runall.sh
+++ runall.sh
@@ -1,10 +1,15 @@
-#!/bin/bash -e
+#!/bin/bash -exv
+
 OLDDIR=$(pwd)
-cd /tmp/
-wget -q --no-check-certificate https://github.com/chb/sample_ccdas/archive/master.tar.gz
-tar xzvf master.tar.gz
+
+if [ ! -e /tmp/sample_ccdas-master ]; then
+    cd /tmp/
+    wget -q --no-check-certificate https://github.com/chb/sample_ccdas/archive/master.tar.gz -O master.tar.gz
+    tar xzvf master.tar.gz
+fi
 
 cd ${OLDDIR}/src
+
 ls -1tr | grep -v ^\\. |\
    grep -v "CcdAnalysis.groovy" | \
-   while read -r i; do groovy "$i" ~/tmp/sample_ccdas-master; done
+   while read -r i; do groovy "$i" /tmp/sample_ccdas-master; done
