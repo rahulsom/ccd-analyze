7fc0ac7fd7a207baa88d0f50dc6b4c6f21266926
Rahul Somasunderam <rsom@certifydatasystems.com>
2014-10-28 13:06:58 -0700

    Fixd PWD before running scripts

--- runall.sh
+++ runall.sh
@@ -1,9 +1,10 @@
 #!/bin/bash -e
+OLDDIR=$(pwd)
 cd /tmp/
 wget -q --no-check-certificate https://github.com/chb/sample_ccdas/archive/master.tar.gz
 tar xzvf master.tar.gz
 
-cd src
+cd ${OLDDIR}/src
 ls -1tr | grep -v ^\\. |\
    grep -v "CcdAnalysis.groovy" | \
    while read -r i; do groovy "$i" ~/tmp/sample_ccdas-master; done
