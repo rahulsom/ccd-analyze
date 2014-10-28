df811f5bbd091260b49df7972355dd1cda0acd7b
Rahul Somasunderam <rsom@certifydatasystems.com>
2014-10-28 15:00:22 -0700

    Runall writes output

--- runall.sh
+++ runall.sh
@@ -1,15 +1,26 @@
 #!/bin/bash -e
 
 OLDDIR=$(pwd)
+DATADIR=$1
 
-if [ ! -e /tmp/sample_ccdas-master ]; then
-    cd /tmp/
-    wget -q --no-check-certificate https://github.com/chb/sample_ccdas/archive/master.tar.gz -O master.tar.gz
-    tar xzf master.tar.gz
+if [ "$DATADIR" = "" ]; then
+    if [ ! -e /tmp/sample_ccdas-master ]; then
+        cd /tmp/
+        wget -q --no-check-certificate https://github.com/chb/sample_ccdas/archive/master.tar.gz -O master.tar.gz
+        tar xzf master.tar.gz
+    fi
+    DATADIR=/tmp/sample_ccdas-master
 fi
 
-cd ${OLDDIR}/src
+cd ${OLDDIR}
+
+if [ -e out ]; then
+    rm -rf out
+fi
+mkdir -p out/results
+
+cd src
 
 ls -1tr | grep -v ^\\. |\
    grep -v "CcdAnalysis.groovy" | \
-   while read -r i; do groovy "$i" /tmp/sample_ccdas-master; done
+   while read -r i; do groovy "$i" ${DATADIR} > ../out/results/${i}.out ; done
