26c3b1589421349dbba149b77f97d45bda3b7e2d
bjong-nliven <brian.jong@nliven.com>
2014-10-29 15:42:30 -0700

    Update HistoryFamily.groovy
    Added counters to total valid CCDs and entries

--- src/HistoryFamily.groovy
+++ src/HistoryFamily.groovy
@@ -10,17 +10,23 @@
     Result
     Family Member count - 2933
     Illness count - 4515
-
+    
+    total entries - 4515
+    total family history CCDs - 2056
+    total valid family history CCDs - 1398
  */
 
 @Grab("org.codehaus.gpars:gpars:1.2.1")
 import groovyx.gpars.GParsPool
+import groovy.transform.BaseScript
+
+@BaseScript CcdAnalysis script
 
 int familyMemberCount = 0
 int familyStatusCount = 0 //no statuses found... unless StatusCode was it...
 int familyIllnessCount = 0
 
-def allTables = [familyMember:[:], familyIllness:[:]]
+def allTables = [familyMember:[:], familyIllness:[:], totalEntries:0, totalCcds:0, totalValidCcds:0]
 def arrayOfFiles = new File("/Users/brian.jong/desktop/certify-ccds").listFiles()
 
 /*
@@ -31,6 +37,7 @@ GParsPool.withPool {
 
         def xml = new XmlSlurper().parse(file.newInputStream())
         def sections = xml.component.structuredBody.component.section
+        def ccdWithValidEntries = false
 
         sections.each { section ->
             def sectionName = section.code.@code.text()
@@ -38,6 +45,8 @@ GParsPool.withPool {
                 def entries = section.entry;
 
                 synchronized (allTables) {
+                    allTables['totalCcds']++
+                    allTables['totalEntries'] += entries.size()
                     //History: Family History
                     entries.each { entry ->
                         def relatedSubject = entry.organizer.subject.relatedSubject
@@ -67,11 +76,15 @@ GParsPool.withPool {
                                 allTables['familyIllness'][item] = 1
                             }
                         }
+
+                        ccdWithValidEntries |= (familyMemberText || illnessText)
                     }
                 }
 
             }
         }
+        if(ccdWithValidEntries)
+            allTables['totalValidCcds']++
     }
 }
 
@@ -79,13 +92,15 @@ allTables['familyMember'].each { k, v ->
     familyMemberCount += v
     println "$k - $v"
 }
-println()
-println "Family Member count - ${familyMemberCount}"
-println()
-
 allTables['familyIllness'].each { k, v ->
     familyIllnessCount += v
     println "$k - $v"
 }
+
 println()
+println "Family Member count - ${familyMemberCount}"
 println "Illness count - ${familyIllnessCount}"
+println "total entries - ${allTables['totalEntries']}"
+println "total family history CCDs - ${allTables['totalCcds']}"
+println "total valid family history CCDs - ${allTables['totalValidCcds']}"
+println()
