5645bb26b6e93f5c139d5540f226cfd328e827ee
bjong-nliven <brian.jong@nliven.com>
2014-10-29 15:39:12 -0700

    Update HistorySocial.groovy
    added extra counts to the total valid ccds and # of entries

--- src/HistorySocial.groovy
+++ src/HistorySocial.groovy
@@ -9,15 +9,21 @@
     Result
     Use count - 7661
     Frequency count - 398
+    total entries - 9052
+    total social history CCDs - 2411
+    total valid social history CCDs - 2025
 */
 
 @Grab("org.codehaus.gpars:gpars:1.2.1")
 import groovyx.gpars.GParsPool
+import groovy.transform.BaseScript
+
+@BaseScript CcdAnalysis script
 
 int useCount = 0
 int frequencyCount = 0
 
-def allTables = [socialUse:[:], socialFrequency:[:]]
+def allTables = [socialUse:[:], socialFrequency:[:], totalEntries:0, totalCcds:0, totalValidCcds:0]
 def arrayOfFiles = new File("/Users/brian.jong/desktop/certify-ccds").listFiles()
 
 /*
@@ -28,6 +34,7 @@ GParsPool.withPool {
 
         def xml = new XmlSlurper().parse(file.newInputStream())
         def sections = xml.component.structuredBody.component.section
+        def ccdWithValidEntries = false
 
         sections.each { section ->
             def sectionName = section.code.@code.text()
@@ -35,11 +42,15 @@ GParsPool.withPool {
             //History: Social
             if (sectionName == "29762-2") {
                 def entries = section.entry;
-                entries.each { entry ->
-                    def observation = entry.observation
-                    synchronized (allTables) {
+                synchronized (allTables) {
+                    allTables['totalCcds']++
+                    allTables['totalEntries'] += entries.size()
+
+                    entries.each { entry ->
+                        def observation = entry.observation
+
                         //look for social name
-                        String socialText = observation.text.text()
+                        String socialText = observation.text.text().trim()
 
                         if (socialText) {
                             def socialItem = socialText.toLowerCase().trim();
@@ -50,7 +61,7 @@ GParsPool.withPool {
                             }
                         }
 
-                        String frequency = observation.value.text()
+                        String frequency = observation.value.text().trim()
 
                         if (frequency) {
                             def socialItem = frequency.toLowerCase().trim();
@@ -60,10 +71,15 @@ GParsPool.withPool {
                                 allTables['socialFrequency'][socialItem] = 1
                             }
                         }
+
+                        ccdWithValidEntries |= (socialText || frequency)
                     }
+
                 }
             }
         }
+        if(ccdWithValidEntries)
+            allTables['totalValidCcds']++
     }
 }
 
@@ -72,12 +88,14 @@ allTables['socialUse'].each { k, v ->
     println "$k - $v"
 }
 println()
-println "Use count - ${useCount}"
-println()
-
 allTables['socialFrequency'].each { k, v ->
     frequencyCount += v
     println "$k - $v"
 }
 println()
+println "Use count - ${useCount}"
 println "Frequency count - ${frequencyCount}"
+println "total entries - ${allTables['totalEntries']}"
+println "total social history CCDs - ${allTables['totalCcds']}"
+println "total valid social history CCDs - ${allTables['totalValidCcds']}"
+println()
