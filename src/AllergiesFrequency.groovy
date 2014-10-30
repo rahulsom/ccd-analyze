e06eb363167aa7c550f084e328b89d8811c9e628
sshih <shannon.shih@nliven.com>
2014-10-30 11:54:07 -0700

    Update AllergiesFrequency.groovy

--- src/AllergiesFrequency.groovy
+++ src/AllergiesFrequency.groovy
@@ -17,11 +17,10 @@ import groovy.transform.BaseScript
 
 @BaseScript CcdAnalysis script
 
-def allergyCounters=[numberOfCCDWithAllergies:0, nullAllergies:0, noKnownAllergies:0, knownAllergies:0, allAllergyRecords:0, allergyStartDateCount:0, allergyEndDateCount:0, allergyObservationCount:0]
+def allergyCounters=[numberOfCCDWithAllergies:0, nullRefAllergies:0, noKnownAllergies:0, knownAllergies:0, allAllergyRecords:0, allergyStartDateCount:0, allergyEndDateCount:0, allergyObservationCount:0]
 
 GParsPool.withPool {
     files.eachParallel { File file ->
-
         def xml = new XmlSlurper().parse(file.newInputStream())
         def sections = xml.component.structuredBody.component.section
 
@@ -29,50 +28,66 @@ GParsPool.withPool {
             def sectionName = section.code.@code.text()
             synchronized (allergyCounters) {
                 // per ccd allergy section
-                if(sectionName == "48765-2"){
+                if (sectionName == "48765-2") {
 //                  println 'CCDFile ' + file.getName() + ' ----------------------';
                     boolean firstTimeInCCDFile = true;
                     //allergy entries
                     def entries = section.entry;
-                    if (entries.size() != 0){
+                    if (entries.size() != 0) {
                         entries.each { entry ->
                             //information about allergies
                             def observation = entry.act.entryRelationship.observation;
-                            String allergyName = observation.participant.participantRole.playingEntity.code.@displayName.text()
-                            if (allergyName){
-                                if(!allergyName.contains("NO KNOWN")) {
-                                    //println allergyName
-                                    allergyCounters['knownAllergies']++;
+                            String referenceValue = observation.participant.participantRole.playingEntity.code.originalText.reference.@value.text().replace('#', '')
+//                                println 'REF: ' + referenceValue
 
-                                    if (firstTimeInCCDFile) {
-                                        allergyCounters['numberOfCCDWithAllergies']++;
-                                        firstTimeInCCDFile = false;
+                            if(referenceValue){
+                                //search for matching allergy title
+                                def textBody = section.text.table.tbody
+                                def usdAllergyRef = textBody.depthFirst().find { it.@ID.text().equalsIgnoreCase(referenceValue) }
+                                if (usdAllergyRef) {
+                                    def allergyName = usdAllergyRef.text()
+                                    if (allergyName) {
+                                        if (!allergyName.contains("NO KNOWN")) {
+//                                                println allergyName
+                                            allergyCounters['knownAllergies']++;
+
+                                            if (firstTimeInCCDFile) {
+                                                allergyCounters['numberOfCCDWithAllergies']++;
+                                                firstTimeInCCDFile = false;
+                                            }
+                                            if (observation.effectiveTime.low.@value.text()) {
+                                                allergyCounters['allergyStartDateCount']++;
+                                            }
+                                            if (observation.effectiveTime.high.@value.text()) {
+                                                allergyCounters['allergyEndDateCount']++;
+                                            }
+
+                                            //information about the observation
+                                            def allergyReactionCode = observation.entryRelationship.observation.text.reference.@value.text();
+                                            if (allergyReactionCode.contains('allergy_reaction')) {
+                                                def allergyReactionObservation = observation.entryRelationship.observation.text.text();
+                                                if (allergyReactionObservation) {
+                                                    //println 'Allergy Reaction: ' + allergyReactionObservation;
+                                                    allergyCounters['allergyObservationCount']++;
+                                                }
+                                            }
+
+                                        } else {
+                                            allergyCounters['noKnownAllergies']++;
+                                        }
+                                    } else {
+                                        //empty allergy names
+                                        allergyCounters['nullRefAllergies']++;
                                     }
-                                    if (observation.effectiveTime.low.@value.text()) {
-                                        allergyCounters['allergyStartDateCount']++;
-                                    }
-                                    if (observation.effectiveTime.high.@value.text()) {
-                                        allergyCounters['allergyEndDateCount']++;
-                                    }
+                                } else {
+                                    //reference value could not finding matching title
+                                    allergyCounters['nullRefAllergies']++;
                                 }
-                                else{
-                                    allergyCounters['noKnownAllergies']++;
-                                }
-                            }
-                            else{
-                                allergyCounters['nullAllergies']++;
+                            }else{
+                                //reference value is null
+                                allergyCounters['nullRefAllergies']++;
                             }
                             allergyCounters['allAllergyRecords']++;
-
-                            //information about the observation
-                            def allergyReactionCode = observation.entryRelationship.observation.text.reference.@value.text();
-                            if(allergyReactionCode.contains('allergy_reaction')){
-                                def allergyReactionObservation = observation.entryRelationship.observation.text.text();
-                                if(allergyReactionObservation){
-                                    //println 'Allergy Reaction: ' + allergyReactionObservation;
-                                    allergyCounters['allergyObservationCount']++;
-                                }
-                            }
                         }
                     }
                 }
@@ -87,7 +102,7 @@ println 'Number of CCDS with Allergies: ' + allergyCounters.get('numberOfCCDWith
 println 'Average number of Allergies per CCD: ' + allergyCounters.get('knownAllergies')/allergyCounters.get('numberOfCCDWithAllergies');
 println ''
 println 'All Allergy Records: ' + allergyCounters.get('allAllergyRecords')
-println 'Number of Null Allergies: ' + allergyCounters.get('nullAllergies')
+println 'Number of Null Ref Allergies: ' + allergyCounters.get('nullRefAllergies')
 println 'Number of No Known Allergies: ' + allergyCounters.get('noKnownAllergies')
 println 'Number of Known Allergies: ' + allergyCounters.get('knownAllergies')
 println 'Number of Allergy Start Dates: ' + allergyCounters.get('allergyStartDateCount')
@@ -95,16 +110,16 @@ println 'Number of Allergy End Dates: ' + allergyCounters.get('allergyEndDateCou
 println 'Number of Allergy Reaction Observation: ' + allergyCounters.get('allergyObservationCount')
 
 
-/* 10/29/2014 -
+/* 10/30/2014 -
 -------------- RESULTS: --------------
-Number of CCDS with Allergies: 3685
-Average number of Allergies per CCD: 2.4377204885
+Number of CCDS with Allergies: 3738
+Average number of Allergies per CCD: 2.4459604066
 
 All Allergy Records: 16390
-Number of Null Allergies: 6181
+Number of Null Ref Allergies: 6021
 Number of No Known Allergies: 1226
-Number of Known Allergies: 8983
-Number of Allergy Start Dates: 505
+Number of Known Allergies: 9143
+Number of Allergy Start Dates: 520
 Number of Allergy End Dates: 0
-Number of Allergy Reaction Observation: 944
+Number of Allergy Reaction Observation: 942
  */
